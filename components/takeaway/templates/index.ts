8532880e8a5d3229fdc874f9f629b92008d03478
palomo <pgonzalojose@gmail.com>
2026-01-14 17:58:36 -0300

    UPDATE: index.ts of template and and compactTemplate

--- components/takeaway/templates/index.ts
+++ components/takeaway/templates/index.ts
@@ -3,12 +3,13 @@
 import GridTemplate from './GridTemplate'
 import CarouselTemplate from './CarouselTemplate'
 import FeaturedTemplate from './FeaturedTemplate'
+import CompactTemplate from './CompactTemplate'
 
 export const CATEGORY_TEMPLATES = {
     grid: GridTemplate,       // Grid estándar 3 columnas
     carousel: CarouselTemplate, // Scroll horizontal
     featured: FeaturedTemplate, // Cards grandes destacadas
-    compact: GridTemplate      // Grid compacto 4 columnas
+    compact: CompactTemplate      // Grid compacto 4 columnas
 }
 
 export type TemplateType = keyof typeof CATEGORY_TEMPLATES
